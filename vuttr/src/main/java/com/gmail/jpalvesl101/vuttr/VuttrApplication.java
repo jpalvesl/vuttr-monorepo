bf7a8c3c886ecf21f41fa34f248da48f68e06890
João Lima <jpalvesl101@gmail.com>
2025-02-21 16:01:19 -0300

    feat: cria projeto do backend

---
+++ vuttr/src/main/java/com/gmail/jpalvesl101/vuttr/VuttrApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.jpalvesl101.vuttr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuttrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuttrApplication.class, args);
+	}
+
+}
